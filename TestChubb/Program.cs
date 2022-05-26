e7b9a2b6ef0cea18becb4d5547bbdd596a3a01d8
ajaykumarchepuri1 <ajaykumarchepuri1@gmail.com>
2022-05-26 17:56:09 +0530

    Add project files.

---
+++ TestChubb/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace TestChubb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pal = "32113";
+            var count = pal.Length;
+            var tempString = string.Empty;
+            var result = 0;
+            for (int i = 0; i <= count; i++)
+            {
+                var temp = pal.ToCharArray();
+                if (i <= temp.Length - 1)
+                {
+                    var currentvalue = temp[i];
+                    tempString += currentvalue;
+                    for (int j = i + 1; j <= count; j++)
+                    {
+                        if (j <= temp.Length - 1)
+                        {
+                            var sencondVar = temp[j];
+                            tempString = tempString + sencondVar.ToString();
+                            if (tempString.Equals(RString(tempString)))
+                            {
+                                result = result + 1;
+                            }
+                        }
+
+                    }
+                    tempString = string.Empty;
+                }
+            }
+            Console.WriteLine("Count  :" + result);
+        }
+
+        public static string RString(string value)
+        {
+            var result = new string(value.ToCharArray().Reverse().ToArray());
+            return result;
+        }
+    }
+}
